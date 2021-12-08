f8dfeee12807a005e82e395cd5fd573ba5bf24c3
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-08 10:32:40 +0100

    les fonctions anonyme et flechee

--- js/app.js
+++ js/app.js
@@ -2,7 +2,21 @@ const alerte = document.getElementById("alert");
 const somme = document.getElementById("somme");
 
 alerte.addEventListener("click", () => {
-    alert ("Hello world");
+    alert("Hello world");
+})
+
+alerte.addEventListener("mouseenter", () => {
+    alerte.style.transform = "scale";
+})
+
+alerte.addEventListener("mouseleave", () => {
+    alerte.style.background = "cyan";
+})
+
+alerte.addEventListener("keypress",(event)=> {
+    if (event.key === "enter") {
+        alert("Hello world");
+    }
 })
 
 somme.addEventListener("click", () => {
@@ -11,3 +25,16 @@ somme.addEventListener("click", () => {
     document.body.appendChild(div);
 })
 
+somme.addEventListener("mouseenter", () => {
+    somme.style.transform = "scale(1)";
+})
+
+somme.addEventListener("mouseleave", () => {
+    somme.style.background = "pink";
+})
+
+
+
+// mouseenter ==>ou lon entre dans la zone dans param event
+// mouseleave ==> ou lon sort
+// clicket
